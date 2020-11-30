86da07097ff11f879f1daad76fd05fed74d0c565
hana88888 <naoki.may.28th@gmail.com>
2020-11-30 23:18:09 +0900

    Add files via upload

---
+++ pardot-api.py
@@ -0,0 +1,27 @@
+#wrapperをインストールする
+pip install pypardot4
+
+#PardotAPIを簡単に使うためのimport
+from pypardot.client import PardotAPI
+
+#jsonを処理するためのimport
+import pandas as pd
+import json
+
+#認証のための変数を入れる
+p = PardotAPI(
+    email ='自分のメールアドレス',
+    password ='自分のパスワード',
+    user_key='自分のuser_key'
+)
+
+#認証する
+p.authenticate()
+
+#昨日以降に作成されたプロスペクトを抽出
+prospects = p.prospects.query(created_after='yesterday')
+total = prospects['total_results']
+for prospect in prospects['prospect']:
+	#1名ずつ名前を抽出
+    print(prospect.get('first_name'))
+ 
